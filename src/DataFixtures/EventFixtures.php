4326a9d4b294555976bd9bb5ea936d5d06b00cbe
MarieVincens <marievincens@yahoo.fr>
2019-07-01 12:46:25 +0200

    Ajout de Security Pack

--- src/DataFixtures/EventFixtures.php
+++ src/DataFixtures/EventFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Event;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,7 +10,20 @@ class EventFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
+        $event1 = new Event();
+        $event1->setTitle();
+        $event1->setSlug();
+        $event1->setPicture();
+        $event1->setDateStart();
+        $event1->setDateEnd();
+        $event1->setDescription();
+        $event1->setPrice();
+        $event1->setUrl();
+        $event1->setUser($this->getReference(""));
+        $event1->setCity($this->getReference(""));
+        $event1->addLanguage();
+
+
         // $manager->persist($product);
 
         $manager->flush();
