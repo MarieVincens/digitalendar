5d834327e795fd155c2a5e8bea9495d6cc87afc6
MarieVincens <marievincens@yahoo.fr>
2019-07-01 14:29:49 +0200

    Suite et fin des fixtures

--- src/DataFixtures/EventFixtures.php
+++ src/DataFixtures/EventFixtures.php
@@ -4,10 +4,12 @@ namespace App\DataFixtures;
 
 use App\Entity\Event;
 use App\Service\Slugger;
+
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class EventFixtures extends Fixture
+class EventFixtures extends Fixture implements DependentFixtureInterface
 {
 
     public function load(ObjectManager $manager)
@@ -23,7 +25,9 @@ class EventFixtures extends Fixture
         $event1->setCity($this->getReference("city-paris"));
         $event1->addLanguage($this->getReference("language-anglais"));
         $event1->addLanguage($this->getReference("language-francais"));
+        $event1->setIsValid(1);
         $manager->persist($event1);
+        $this->setReference("event-1", $event1);
 
         $event2 = new Event();
         $event2->setTitle("Event numero 2");
@@ -37,7 +41,9 @@ class EventFixtures extends Fixture
         $event2->setCity($this->getReference("city-rennes"));
         $event2->addLanguage($this->getReference("language-italien"));
         $event2->addLanguage($this->getReference("language-francais"));
+        $event2->setIsValid(1);
         $manager->persist($event2);
+        $this->setReference("event-2", $event2);
 
         $event3 = new Event();
         $event3->setTitle("Event numero 3");
@@ -49,9 +55,25 @@ class EventFixtures extends Fixture
         $event3->setUser($this->getReference("user-pierre"));
         $event3->setCity($this->getReference("city-marseilles"));
         $event3->addLanguage($this->getReference("language-italien"));
+        $event3->setIsValid(1);
         $manager->persist($event3);
-
+        $this->setReference("event-3", $event3);
 
         $manager->flush();
+    }
+
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on
+     *
+     * @return array
+     */
+    public function getDependencies(){
+
+        return [
+            UserFixtures::class,
+            CityFixtures::class,
+            LanguageFixtures::class
+        ];
     }
 }
--- src/DataFixtures/ParticipantFixtures.php
+++ src/DataFixtures/ParticipantFixtures.php
@@ -4,48 +4,63 @@ namespace App\DataFixtures;
 
 use App\Entity\Participant;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class ParticipantFixtures extends Fixture
+class ParticipantFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
         $p1 = new Participant();
-        $p1->setCreatedAt();
-        $p1->setUser();
-        $p1->addEvent();
+        $p1->setCreatedAt(new \DateTime("2017-03-14"));
+        $p1->setUser($this->getReference("user-marie"));
+        $p1->addEvent($this->getReference("event-1"));
+        $p1->addEvent($this->getReference("event-2"));
+        $p1->addEvent($this->getReference("event-3"));
         $manager->persist($p1);
 
         $p2 = new Participant();
-        $p2->setCreatedAt();
-        $p2->setUser();
-        $p2->addEvent();
+        $p2->setCreatedAt(new \DateTime("2018-01-14"));
+        $p2->setUser($this->getReference("user-maite"));
+        $p2->addEvent($this->getReference("event-1"));
+        $p2->addEvent($this->getReference("event-3"));
         $manager->persist($p2);
 
         $p3 = new Participant();
-        $p3->setCreatedAt();
-        $p3->setUser();
-        $p3->addEvent();
+        $p3->setCreatedAt(new \DateTime("2019-02-06"));
+        $p3->setUser($this->getReference("user-jerome"));
+        $p3->addEvent($this->getReference("event-1"));
         $manager->persist($p3);
 
         $p4 = new Participant();
-        $p4->setCreatedAt();
-        $p4->setUser();
-        $p4->addEvent();
+        $p4->setCreatedAt(new \DateTime("2017-12-01"));
+        $p4->setUser($this->getReference("user-pierre"));
+        $p4->addEvent($this->getReference("event-2"));
         $manager->persist($p4);
 
         $p5 = new Participant();
-        $p5->setCreatedAt();
-        $p5->setUser();
-        $p5->addEvent();
+        $p5->setCreatedAt(new \DateTime("2017-06-23"));
+        $p5->setUser($this->getReference("user-tintin"));
+        $p5->addEvent($this->getReference("event-2"));
+        $p5->addEvent($this->getReference("event-3"));
         $manager->persist($p5);
 
         $p6 = new Participant();
-        $p6->setCreatedAt();
-        $p6->setUser();
-        $p6->addEvent();
+        $p6->setCreatedAt(new \DateTime("2017-09-30"));
+        $p6->setUser($this->getReference("user-quirrel"));
+        $p6->addEvent($this->getReference("event-1"));
+        $p6->addEvent($this->getReference("event-2"));
+        $p6->addEvent($this->getReference("event-3"));
         $manager->persist($p6);
 
         $manager->flush();
+    }
+
+    public function getDependencies(){
+
+        return [
+            UserFixtures::class,
+            EventFixtures::class
+        ];
     }
 }
