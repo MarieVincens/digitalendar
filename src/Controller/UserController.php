3e999b1efd73e74000975c453e4ae72052e093db
MarieVincens <marievincens@yahoo.fr>
2019-07-02 11:52:52 +0200

    début des CRUD

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/user/{name}", name="user")
      */
-    public function index()
+    public function index(User $user)
     {
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'user' => $user,
         ]);
     }
+
+    /**
+     * @Route("/user/{name}/edit", name="user_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, User $user): Response
+    {
+        $form = $this->createForm(EventType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('event_index', [
+                'id' => $event->getId(),
+            ]);
+        }
+
+        return $this->render('event/edit.html.twig', [
+            'event' => $event,
+            'form' => $form->createView(),
+        ]);
+    }
+
 }
