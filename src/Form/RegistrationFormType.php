122bc92cbff4e1c336b6ff6146cd33173c0be9c2
MarieVincens <marievincens@yahoo.fr>
2019-07-04 17:01:14 +0200

    Compter les participants

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,6 +18,7 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('username')
+            ->add('email')
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -35,7 +36,7 @@ class RegistrationFormType extends AbstractType
 
                 ],
             ])
-            ->add('email');
+
         ;
     }
 
